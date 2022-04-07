b3787e7983fe691a45e64335fe02d6dca1d04480
SomeBrujah <wissenseeker@gmail.com>
2022-04-07 16:46:25 +0300

    Add statistic

--- world.js
+++ world.js
@@ -23,13 +23,13 @@ class World {
         const stat = {
             population: this.population.length,
             inAnotherWorld: this.anotherWorld.length,
-            women: this.population.filter((person)=>{ return person instanceof Woman}).length,
-            men:this.population.filter((person)=>{ return person instanceof Man}).length,
-            brown:this.population.filter((person)=>{ return person.eyeColor === 'brown'}).length,
-            green:this.population.filter((person)=>{ return person.eyeColor === 'green'}).length,
-            gray:this.population.filter((person)=>{ return person.eyeColor === 'gray'}).length,
-            blue:this.population.filter((person)=>{ return person.eyeColor === 'blue'}).length,
-            hazel:this.population.filter((person)=>{ return person.eyeColor === 'hazel'}).length,
+            women: this.population.filter((person) => { return person instanceof Woman }).length,
+            men: this.population.filter((person) => { return person instanceof Man }).length,
+            brown: this.population.filter((person) => { return person.eyeColor === 'brown' }).length,
+            green: this.population.filter((person) => { return person.eyeColor === 'green' }).length,
+            grey: this.population.filter((person) => { return person.eyeColor === 'grey' }).length,
+            blue: this.population.filter((person) => { return person.eyeColor === 'blue' }).length,
+            hazel: this.population.filter((person) => { return person.eyeColor === 'hazel' }).length,
         }
         return stat;
     }
